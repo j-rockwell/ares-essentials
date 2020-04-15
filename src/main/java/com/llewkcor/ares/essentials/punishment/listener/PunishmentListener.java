4d18b9a630206710bb8e60a0409c86041e664e49
j-rockwell <jrobertrockwell@gmail.com>
2020-04-14 23:32:27 -0700

    Print alt counts upon a player connecting

--- src/main/java/com/llewkcor/ares/essentials/punishment/listener/PunishmentListener.java
+++ src/main/java/com/llewkcor/ares/essentials/punishment/listener/PunishmentListener.java
@@ -1,12 +1,16 @@
 package com.llewkcor.ares.essentials.punishment.listener;
 
 import com.llewkcor.ares.commons.event.ProcessedChatEvent;
+import com.llewkcor.ares.commons.util.bukkit.Scheduler;
 import com.llewkcor.ares.commons.util.general.IPS;
+import com.llewkcor.ares.core.alts.data.AltEntry;
 import com.llewkcor.ares.essentials.punishment.PunishmentManager;
 import com.llewkcor.ares.essentials.punishment.data.Punishment;
 import com.llewkcor.ares.essentials.punishment.data.PunishmentType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,6 +29,7 @@ public final class PunishmentListener implements Listener {
         final UUID uniqueId = event.getUniqueId();
         final long address = IPS.toLong(event.getAddress().getHostAddress());
         final Collection<Punishment> activePunishments = manager.getActivePunishments(uniqueId, address);
+        final Collection<AltEntry> altEntries = manager.getPlugin().getCore().getAltManager().getAlts(uniqueId, address);
 
         for (Punishment punishment : activePunishments) {
             if (punishment.getPunishmentType().equals(PunishmentType.BLACKLIST)) {
@@ -36,6 +41,12 @@ public final class PunishmentListener implements Listener {
                 event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_BANNED, manager.getKickMessage(punishment));
                 return;
             }
+        }
+
+        if (altEntries.size() > 1) {
+            new Scheduler(manager.getPlugin()).sync(() -> Bukkit.getOnlinePlayers().stream().filter(player -> player.hasPermission("eessentials.punishment.lookup")).forEach(staff -> {
+                staff.sendMessage(ChatColor.RED + "[Alt Detected] " + ChatColor.DARK_PURPLE + event.getName() + ChatColor.GRAY + " has " + ChatColor.LIGHT_PURPLE + altEntries.size() + ChatColor.GRAY + " connected accounts. Type " + ChatColor.AQUA + "/lookup " + event.getName() + ChatColor.GRAY + " to view");
+            })).run();
         }
     }
 
