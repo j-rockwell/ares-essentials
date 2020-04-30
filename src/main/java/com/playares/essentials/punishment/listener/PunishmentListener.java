326aa3869e83c45e4b030bbce4f9a7f383bf7359
j-rockwell <jrobertrockwell@gmail.com>
2020-04-30 13:42:31 -0700

    Added alt detection notification

--- src/main/java/com/playares/essentials/punishment/listener/PunishmentListener.java
+++ src/main/java/com/playares/essentials/punishment/listener/PunishmentListener.java
@@ -1,12 +1,16 @@
 package com.playares.essentials.punishment.listener;
 
+import com.playares.commons.services.alts.event.AltDetectEvent;
 import com.playares.essentials.punishment.PunishmentManager;
 import com.playares.essentials.punishment.data.Punishment;
 import com.playares.essentials.punishment.data.PunishmentType;
 import com.playares.commons.event.ProcessedChatEvent;
 import com.playares.commons.util.general.IPS;
+import com.playares.essentials.staff.data.StaffAccount;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,6 +23,24 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public final class PunishmentListener implements Listener {
     @Getter public final PunishmentManager manager;
+
+    @EventHandler
+    public void onAltDetected(AltDetectEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+
+        final Player player = event.getPlayer();
+
+        manager.getEssentials().getStaffManager().getAccountByPermission(StaffAccount.StaffSetting.SHOW_ALT_NOTIFICATIONS, true).forEach(staffAccount -> {
+            final Player staff = Bukkit.getPlayer(staffAccount.getUniqueId());
+
+            if (staffAccount.isVerified()) {
+                staff.sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "Alt" + ChatColor.GRAY + "] " + ChatColor.RED + player.getName() + ChatColor.GRAY + " has " + ChatColor.BLUE + event.getSessions() + ChatColor.GRAY + " sessions tied to their account");
+                staff.sendMessage(ChatColor.GRAY + "Type " + ChatColor.RED + "/lookup " + player.getName() + ChatColor.GRAY + " to further investigate");
+            }
+        });
+    }
 
     @EventHandler
     public void onLoginAttempt(AsyncPlayerPreLoginEvent event) {
